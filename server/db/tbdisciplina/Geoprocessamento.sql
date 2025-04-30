6c1c7309ae6155445113871b3e83b31819bdc8d1
nickdsm24 <nicolas.silva67@fatec.sp.gov.br>
2025-04-29 21:21:05 -0300

    novos dados

---
+++ server/db/tbdisciplina/Geoprocessamento.sql
@@ -0,0 +1,47 @@
+insert into tbdisciplina (nome)
+values
+ 
+-- semestre 1
+ 
+('Introdução à Ciência da Geoinfomação'),
+('Algoritmos e Lógica de Programação'),
+('Metodologia'),
+('Desenho Técnico'),
+('Comunicação'),
+('Inglês I'),
+('Fundamentos de Física'),
+('Cálculo'),
+ 
+-- semestre 3
+ 
+('Estatística'),
+('Processamento Digital de Imagens'),
+('Inglês III'),
+('Geodésia'),
+('Topografia e Batimetria'),
+('Modelagem de Banco de Dados'),
+('Linguagem de Programação II'),
+ 
+-- semestre 5
+ 
+('Análise Ambiental'),
+('Fundamentos da administração'),
+('Legislação'),
+('WebGIS'),
+('Infra URB'),
+('Análise Espacial e MT'),
+('Projetos 1'),
+('Inglês V'),
+ 
+-- semestre 6
+ 
+('Posicionamento por Satélite'),
+('Fotogrametria Analógica Digital'),
+('Cadastro Técnico Multifinalitário'),
+('Geomarkenting'),
+('Integração e Análise Dados Territoriais'),
+('Inglês VI'),
+('Projeto 2'),
+('GeoReferenciamento Imóveis Rurais'),
+('Padrões Distrib. Dados em SIG'),
+('Integração e Análise Dados Territoriais');
---
+++ server/db/tbdisciplina/MARH (noturno).sql
@@ -0,0 +1,36 @@
+insert into tbdisciplina (nome)
+values
+ 
+-- semestre 1
+ 
+('Ciências Ambientais e das Águas'),
+('Inglês I'),
+('Fundamentos Comunicação Empresarial'),
+('Química Geral'),
+('Metodologia'),
+('Sociologia Ambiental'),
+('Biologia'),
+('Matemática Aplicada'),
+('Geociência Ambiental'),
+ 
+-- semestre 2
+ 
+('Inglês II'),
+('SERE'),
+('Hidrologia e Recursos Hídricos'),
+('Cartografia, Topografia e Batimetria'),
+('Físico Química Ambiental'),
+('Microbiologia'),
+('Ecologia'),
+('Climatologia e Metereologia'),
+ 
+-- semestre 3
+ 
+('Hidráulica Fluvial'),
+('Estatística'),
+('Gestão da Qualidade'),
+('Limnologia'),
+('Saneamento Ambiental 1'),
+('Planejamento e Conservação Ambiental'),
+('IPDI'),
+('Inglês III');
